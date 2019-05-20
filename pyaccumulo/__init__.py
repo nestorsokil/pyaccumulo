1aef8327cfccb1f87746ca3a19e0b70262bd63e2
nsokil <nsokil@kountable.com>
2019-05-20 12:26:23 +0300

    removed async impls from the repo

--- pyaccumulo/__init__.py
+++ pyaccumulo/__init__.py
@@ -1,13 +1,11 @@
 from pyaccumulo.accumulo import *
 from pyaccumulo.simple_pool import AccumuloPool
-from pyaccumulo.aio_pool import AccumuloAsyncPool
 import pyaccumulo.iterators as iterators
 import pyaccumulo.objects as objects
 
 __all__ = [
     "Accumulo",
     "AccumuloPool",
-    "AccumuloAsyncPool",
     "objects",
     "iterators"
 ]
